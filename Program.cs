cc9939617d6df12c50617a76897a62bb6b56bc33
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-17 16:25:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Inserisci il numero di cui vuoi calcolare il fattoriale");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Hai inserito il numero {0} ed il suo fattoriale è {1}", number, Factorial(number));
+Console.ReadKey(true);
+
+int Factorial(int number)
+    {
+        return Enumerable.Range(1, number).Aggregate((a, b) => a * b);
+    }
+
